cff0e1b8cae691bdbc80a54baa7b7e9b080093c6
Recrut-rf <c4wrc@mail.ru>
2021-07-19 09:04:14 +0400

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+
+#include "mywidget.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    // картинки с фигурами - будут как фон для кнопок
+    QPixmap *circlePix_;
+    QPixmap *squarePix;
+    QPixmap *trianglePix;
+
+    // виджеты с фигурами
+    MyWidget *myWidgetCircle;
+    MyWidget *myWidgetSquare;
+    MyWidget *myWidgetTriangle;
+
+
+    // функция создания кнопки из картинки
+    void addButton(QPixmap *pix, QPushButton *button);
+};
+#endif // MAINWINDOW_H
